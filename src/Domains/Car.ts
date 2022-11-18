7115e4ac6fe9d5ac47617ef252daaedda08b330c
Fernanda Vidal <fevidal.dev@gmail.com>
2022-11-18 18:43:43 -0300

    fix: Domain Car.ts.

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -10,12 +10,12 @@ export default class Car {
   private doorsQty: number;
   private seatsQty: number;
 
-  constructor(protected car: ICar) {
+  constructor(car: ICar) {
     this.id = car.id;
     this.model = car.model;
     this.year = car.year;
     this.color = car.color;
-    this.status = car.status;
+    this.status = car.status || false;
     this.buyValue = car.buyValue;
     this.doorsQty = car.doorsQty;
     this.seatsQty = car.seatsQty;
