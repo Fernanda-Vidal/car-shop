91551191a8b3060125bcbf1c09e2110f0f2502b2
Fernanda Vidal <fevidal.dev@gmail.com>
2022-11-18 19:10:55 -0300

    refactor: interfaces.

--- src/Interfaces/ICar.ts
+++ src/Interfaces/ICar.ts
@@ -1,10 +1,8 @@
-export default interface ICar {
-  id?: string;
-  model: string;
-  year: number;
-  color: string;
-  status?: boolean;
-  buyValue: number;
+import IVehicle from './IVehicle';
+
+interface ICar extends IVehicle {
   doorsQty: number;
   seatsQty: number;
 }
+
+export default ICar;
