90cb1a50b60e3809d321302b88bce71776707097
Fernanda Vidal <fevidal.dev@gmail.com>
2022-11-20 11:55:32 -0300

    refactor: testes car.

--- tests/unit/Services/cars.test.ts
+++ tests/unit/Services/cars.test.ts
@@ -5,56 +5,40 @@ import CarService from '../../../src/Services/CarService';
 import { carInput, carList, carOutput, carUpdate, id } from '../../mocks/cars.service.mock';
 
 describe('Testa a rota /cars', function () {
-  describe('Verifica se é possível', function () {
-    afterEach(function () {
-      sinon.restore();
-    });
-    it('Cadastrar um carro com sucesso', async function () {
-      sinon.stub(Model, 'create').resolves(carOutput);
+  afterEach(function () {
+    sinon.restore();
+  });
+  it('Cadastrar um carro com sucesso', async function () {
+    sinon.stub(Model, 'create').resolves(carOutput);
     
-      const service = new CarService();
-      const result = await service.addCar(carInput);
+    const service = new CarService();
+    const result = await service.addCar(carInput);
     
-      expect(result).to.be.deep.equal(carOutput);
-    });
+    expect(result).to.be.deep.equal(carOutput);
+  });
 
-    it('Listar todos os carros cadastrados com sucesso', async function () {
+  describe('Verifica listagem carros', function () {
+    it('É possível listar todos os carros cadastrados com sucesso', async function () {
       sinon.stub(Model, 'find').resolves(carList);
-
+  
       const service = new CarService();
       const result = await service.getAll();
-
+  
       expect(result).to.be.deep.equal(carList);
     });
-
-    it('Listar um carro específico com sucesso', async function () {
+  
+    it('É possível listar um carro específico com sucesso', async function () {
       sinon.stub(Model, 'findById').resolves(carList[0]);
-
+  
       const service = new CarService();
       const result = await service.getById('63780048100ec2f0e8f854a4');
-
+  
       expect(result).to.be.deep.equal(carList[0]);
     });
 
-    it('Atualizar um carro com sucesso', async function () {
-      sinon.stub(Model, 'findById').resolves(carList[0]);
-      sinon.stub(Model, 'findByIdAndUpdate').resolves(carList[3]);
-
-      const service = new CarService();
-      const result = await service.updateCar(id, carUpdate);
-
-      expect(result).to.be.deep.equal(carList[3]);
-    });
-  });
-
-  describe('Verifica se não é possível', function () {
-    afterEach(function () {
-      sinon.restore();
-    });
-    
-    it('Listar um carro com id inválido', async function () {
+    it('Não é possível listar um carro com id inválido', async function () {
       sinon.stub(Model, 'findById').resolves();
-
+  
       try {
         const service = new CarService();
         await service.getById('XXX');
@@ -62,16 +46,49 @@ describe('Testa a rota /cars', function () {
         expect((error as Error).message).to.be.equal('Invalid mongo id');
       }
     });
+  });
 
-    it('Atualizar um carro com um id inválido', async function () {
+  describe('Verifica atualização de um carro', function () {
+    it('É possível atualizar um carro com sucesso', async function () {
+      sinon.stub(Model, 'findById').resolves(carList[0]);
+      sinon.stub(Model, 'findByIdAndUpdate').resolves(carList[3]);
+  
+      const service = new CarService();
+      const result = await service.updateCar(id, carUpdate);
+  
+      expect(result).to.be.deep.equal(carList[3]);
+    });
+
+    it('Não é possível atualizar um carro com um id inválido', async function () {
       sinon.stub(Model, 'findById').resolves(false);
-
+  
       try {
         const service = new CarService();
         await service.updateCar(id, carUpdate);
       } catch (error) {
         expect((error as Error).message).to.be.equal('Car not found');
       }
-    });
+    });  
   });
+
+  describe('Verifica a exclusão de um carro', function () {
+    it('Não é possível deletar um carro inexistente', async function () {
+      sinon.stub(Model, 'deleteOne').resolves({ deletedCount: 0 } as any);
+  
+      try {
+        const service = new CarService();
+        await service.delete('63780048100ec2f0e8f854a4');
+      } catch (error) {
+        expect((error as Error).message).to.be.equal('Car not found');
+      }
+    });
+    //   it('É possível deletar um carro com sucesso', async function () {
+    //     sinon.stub(Model, 'deleteOne').resolves({ deletedCount: 1 } as any);
+  
+    //     const service = new CarService();
+    //     const result = await service.delete('63780048100ec2f0e8f854a4');
+  
+  //     expect(result).to.have.property('deletedCount', 1);
+  //   });
+  });  
 });
